933feec3e5e1f9a8ddcc8aff6e2a1571e18755fd
İsmail Keyvan <58604123+Keyvan14162@users.noreply.github.com>
2020-03-04 20:21:21 +0300

    Add files via upload

---
+++ JAVA/PackageS/src/France/Paris.java
@@ -0,0 +1,40 @@
+package France;
+
+import Turkey.Ankara;
+import Turkey.Kutahya;
+import java.util.Scanner;
+
+
+public class Paris extends Kutahya{
+    
+    public void stdnt(){
+  
+        Ankara main = new Ankara();
+        //with this we can reach main class , main is in Deneme Package
+       
+        Scanner scanner = new Scanner(System. in);
+        
+        Kutahya human = new Kutahya();
+        
+        while(true){
+            
+        System.out.println("Please enter persons informations :");
+        human.read();
+        human.show();
+        
+        System.out.println("For confirm persons informations please type 'confirm'");
+        System.out.println("For rewrite persons informations please type 'rewrite'");
+        String temp = scanner.next();
+        
+        if (temp.equalsIgnoreCase("confirm"))
+            break;
+        else if(temp.equalsIgnoreCase("rewrite"));
+        else
+            System.out.println("Wrong type.Let's do it agagin");
+        }     
+        
+        
+        
+    }
+
+}
